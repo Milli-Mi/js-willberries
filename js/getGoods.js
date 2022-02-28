e066d6fac6a3c04255dd042069bf60b269a84efe
Milli-Mi <87952989+Milli-Mi@users.noreply.github.com>
2022-02-28 04:04:02 +0300

    added filter method

--- js/getGoods.js
+++ js/getGoods.js
@@ -1,13 +1,15 @@
 const getGoods = () => {
   const links = document.querySelectorAll('.navigation-link')
   // console.log(links);
-  const getData = () => {
+  const getData = (value, category) => {
     fetch('/db/db.json')
       .then((res) => res.json())
 
       .then((data) => {
         // console.log(data)
-        localStorage.setItem('goods', JSON.stringify(data))
+        const array = data.filter((item) => item[category] === value)
+
+        localStorage.setItem('goods', JSON.stringify(array))
       })
   }
 
@@ -15,24 +17,29 @@ const getGoods = () => {
     link.addEventListener('click', (e) => {
       // console.log('Click')
       e.preventDefault()
-      getData();
+      const linkValue = link.textContent
+      const category = link.dataset.field
+      //console.log(category);
+      // console.log(linkValue);
+      //  console.log(link);
+      getData(linkValue, category)
     })
   })
   //===метод записи
   // localStorage.setItem('goods', {name: 'all'})
   // localStorage.setItem('goods', JSON.stringify({name: 'all'}))
-  
+
   localStorage.setItem('goods', JSON.stringify([1, 2, 3, 4]))
   //===метод чтения
   // console.log(localStorage.getItem('goods'));
   // console.log(JSON.parse(localStorage.getItem('goods')));
   const goods = JSON.parse(localStorage.getItem('goods'))
- 
-    //console.log(goods);
-   console.log(localStorage)
+
+  //console.log(goods);
+  //  console.log(localStorage)
   //=== метод удаления
-  localStorage.removeItem('goods');
-  console.log(localStorage);
+  localStorage.removeItem('goods')
+  // console.log(localStorage);
 }
 
 getGoods()
